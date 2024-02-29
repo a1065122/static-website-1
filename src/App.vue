d7c5e2144defb26e799d3a3f7887955f5ded8996
a1065122 <142887026+a1065122@users.noreply.github.com>
2024-02-29 10:04:35 +0800

    vue-靜態網站

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      a: 1,
+      isMounted: true
+    }
+  },
+}
+ 
+</script>
+
+<template>
+  <div class="flex justify-center items-center flex-wrap min-h-[100dvh] bg-black text-white">
+    <RouterView v-if="isMounted" />
+    <div class="w-full flex justify-center">
+      <button class="w-30 p-2 rounded-md bg-sky-600" @click="isMounted = false">點我刪除組件</button>
+    </div>
+  </div>
+  
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  data() {
+    return {
+      a: 1,
+      isMounted: true
+    }
+  },
+  beforeCreate() {
+    console.log('Hook: beforeCreate');
+    console.log(` this.a: ${this.a}`);
+    console.log();
+  },
+  created() {
+    console.log('Hook: created');
+    console.log();
+  },
+  beforeMount() {
+    console.log('Hook: beforeMount');
+    console.log();
+  },
+  mounted() {
+    console.log('Hook: mounted');
+    console.log();
+  },
+  beforeUpdate() {
+    console.log('Hook: beforeUpdate');
+    console.log(` this.a: ${this.a}`);
+    console.log();
+  },
+  updated() {
+    console.log('Hook: updated');
+    console.log();
+  },
+  beforeUnmount() {
+    console.log('Hook: beforeDestroy');
+    console.log();
+  },
+  unmounted() {
+    console.log('Hook: destroyed');
+    console.log();
+  },
+}
+</script>
+
+<template>
+  <main class="">
+  <div class="bg-white text-black pl-2 font-bold rounded-sm mb-3">
+    {{a}}
+  </div>
+  <button class="w-30 p-2 rounded-md bg-sky-600" @click="a++">點我增加a數值</button>
+
+  </main>
+</template>
