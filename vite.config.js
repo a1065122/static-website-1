88d985e0eef7364cdafb8e2be603399eb9dd8899
a1065122 <142887026+a1065122@users.noreply.github.com>
2024-02-29 11:32:51 +0800

    static-website-1

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/',
+  base: '/static-website-1/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
